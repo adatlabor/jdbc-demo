c7046941997efb47167f866d4812e4a3665ae540
Jozsef Marton <jmarton@users.sourceforge.net>
2016-03-20 16:28:19 +0100

    ODJBC.jar linked during deploy, app vendor is the Student.

--- util/deploy.sh
+++ util/deploy.sh
@@ -10,6 +10,8 @@ ARCHIVE_DIRNAME=${REPOSITORY_NAME}-${BRANCH_NAME}
 TARGET_DIRNAME=jdbc
 WEB_DIRNAME=jdbc
 
+OJDBC7_PATH=/oracle/product/12.1.0/dbhome_1/jdbc/lib/ojdbc7.jar
+
 
 TEMPFILE=$( mktemp -t lab5jdbc.XXXXXXXXX )
 wget ${DOWNLOAD_URL} -O $TEMPFILE
@@ -25,6 +27,14 @@ chmod 701 ${TARGET_DIRNAME}
 chmod 705 ${TARGET_DIRNAME}/web
 rm $TEMPFILE
 
+# Symlink ojdbc7.jar driver
+if [ -f ${OJDBC7_PATH} ]; then
+  ln -s ${OJDBC7_PATH} ${TARGET_DIRNAME}/lib/
+  echo "JDBC driver linked to the lib/ directory."
+else
+  echo "WARNING: You need to manually add ojdbc7.jar to the lib/ directory."
+fi
+
 # Preparing public_html
 chmod 701 ${HOMEDIR}
 mkdir -p ${HOMEDIR}/public_html
@@ -37,7 +47,7 @@ cat <<HERE
 Working environment was successfully set!
 
 Do not forget to:
-* add Oracle JDBC driver to the lib/ subdirectory.
+* ensure that Oracle JDBC driver is in the lib/ subdirectory.
 * create and edit conf/preproc.config.sh (by copying preproc.config.sh.sample)
 * change dir to workdir (cd ${TARGET_DIRNAME})
 * run ./util/preproc.sh
