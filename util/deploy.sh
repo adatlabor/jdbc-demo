b2b441290ac523820c2cad2b617fcb7ffffec848
Jozsef Marton <marton@db.bme.hu>
2018-03-13 11:09:54 +0100

    JDBC deploy: work around disabling weak cyrpo @ github
    See: https://githubengineering.com/crypto-removal-notice/

--- util/deploy.sh
+++ util/deploy.sh
@@ -5,7 +5,8 @@ REPOSITORY_NAME=jdbc-demo
 BRANCH_NAME=master
 DOWNLOAD_BASE=https://github.com/adatlabor
 
-DOWNLOAD_URL=${DOWNLOAD_BASE}/${REPOSITORY_NAME}/archive/${BRANCH_NAME}.zip
+#DOWNLOAD_URL=${DOWNLOAD_BASE}/${REPOSITORY_NAME}/archive/${BRANCH_NAME}.zip
+DOWNLOAD_URL=https://www.db.bme.hu/files/szoftlab5/jdbc/master.zip
 ARCHIVE_DIRNAME=${REPOSITORY_NAME}-${BRANCH_NAME}
 TARGET_DIRNAME=jdbc
 WEB_DIRNAME=jdbc
@@ -25,7 +26,7 @@ HERE
 fi
 
 TEMPFILE=$( mktemp -t lab5jdbc.XXXXXXXXX )
-wget ${DOWNLOAD_URL} -O $TEMPFILE
+wget --no-check-certificate ${DOWNLOAD_URL} -O $TEMPFILE
 if [ "$?" != "0" ] ; then
   echo
   echo !!! Failed to download files !!!
